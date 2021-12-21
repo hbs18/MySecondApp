7b5a8e460d159c47b110b1e18e13c887f46d001f
hbs18 <fran.poje@gmail.com>
2021-12-21 15:10:22 +0100

    Implementacija očitavanja senzora akcelerometra

--- app/home/sensor-view-model.js
+++ app/home/sensor-view-model.js
@@ -1,9 +1,22 @@
-import { fromObject } from '@nativescript/core'
+import { fromObject, Observable } from '@nativescript/core'
+import { startAccelerometerUpdates } from 'nativescript-accelerometer'
 
 export function SensorViewModel() {
-  const viewModel = fromObject({
-    /* Add your view model properties here */
-  })
+  const viewModel = new Observable()
+  viewModel.x = 10
+  viewModel.y = 20
+  viewModel.z = 30
+
+
+  var accelerometer = require("nativescript-accelerometer")
+  accelerometer.startAccelerometerUpdates(function(data) {
+    viewModel.set('x', data.x)
+    viewModel.set('y', data.y)
+    viewModel.set('z', data.z)
+  }, { sensorDelay: "ui" }
+  
+  )
+
 
   return viewModel
 }
